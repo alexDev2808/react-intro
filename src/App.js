b6cf6bcd2abb14f6302f547f6cf5be5c64f8a805
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-28 19:40:15 -0600

    Comunicacion entre componentes: props y atributos

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+
+
+const defaultTODOs = [
+  { text: "Aprender React", completed: false },
+  { text: "Aprender Desarrollo Movil", completed: false },
+  { text: "Aprender Python", completed: false },
+  { text: "Aprender Ingles", completed: false },
+]
 
 function App() {
   return (
     <div className="App">
 
-      <TodoCounter />
+      <TodoCounter completed={14} total={23} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTODOs.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+        ))}
       </TodoList>
 
 
-      {/* <CreateTodoButton /> */}
+      <CreateTodoButton />
 
     </div>
   );
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,6 +1,6 @@
-function TodoCounter() {
+function TodoCounter({ total, completed }) {
     return (
-        <h1>Has completado 3 de 5 TODO's</h1>
+        <h1>Has completado { completed } de { total } TODO's</h1>
     )
 }
 
